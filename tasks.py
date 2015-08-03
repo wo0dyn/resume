e450caa37375474a984ac04b6eeec2386ed9b65f
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2015-08-03 15:35:10 +0200

    Add/Refactor several tasks (compile/edit/open)

--- tasks.py
+++ tasks.py
@@ -7,21 +7,44 @@ from invoke import ctask as task
 BASE = 'Nicolas-Dubois-cv-fr.{ext}'
 
 
-@task(default=True)
-def build(context, open_pdf=True):
+@task(default=True, name='build')
+def _build(context):
     """
     Build PDF from LaTeX sources and open it in the default PDF viewer.
     """
-    context.run('pdflatex ' + BASE.format(ext='tex'))
-    context.run('pdflatex ' + BASE.format(ext='tex'))
-    if open_pdf:
-        context.run('open ' + BASE.format(ext='pdf'))
+    _compile(context)
+    _open(context)
 
 
-@task
-def clean(context):
+@task(name='compile')
+def _compile(context):
+    """
+    Compile LaTeX sources.
+    """
+    context.run('pdflatex ' + BASE.format(ext='tex'))
+    context.run('pdflatex ' + BASE.format(ext='tex'))
+
+
+@task(name='open')
+def _open(context):
+    """
+    Open PDF file in default PDF viewer.
+    """
+    context.run('open ' + BASE.format(ext='pdf'))
+
+
+@task(name='clean')
+def _clean(context):
     """
     Remove non-(La)TeX files (temp files and PDFs).
     """
     for f in set(glob(BASE.format(ext='*'))) - set(glob('*.tex')):
         os.remove(f)
+
+
+@task(name='edit')
+def _edit(context):
+    """
+    Edit LaTeX sources.
+    """
+    context.run('open ' + BASE.format(ext='tex'))
